9194b47683eaf0a157ad94aa1e3b6b602ef22bd8
Ashle3 <ashleeb3@byui.edu>
2023-09-30 22:58:33 -0500

    got rid of .trim()

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,7 +11,7 @@ const getAll = async (req, res, next) => {
   };
   
 const getSingle = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id.trim());
+    const userId = new ObjectId(req.params.id);
     const result = await mongodb
       .getDb()
       .db('contacts')
