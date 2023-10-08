a0d3b7ff070b93f8e7624895162cedfb3a3184af
Ashle3 <ashleeb3@byui.edu>
2023-10-07 22:01:22 -0500

    got my api doc working

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./swagger'));
 //first part creates '/contacts' part of the GET request
 router.use('/contacts', require('./contacts'))
 
