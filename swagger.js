09f0c79650127c386f45dd4792a6dc052bbb536b
Ashle3 <ashleeb3@byui.edu>
2023-10-07 22:18:19 -0500

    got rid of http in render link

--- swagger.js
+++ swagger.js
@@ -5,15 +5,11 @@ const doc = {
     title: 'Contacts API',
     description: 'List of people I know.',
   },
-  host: 'https://cse341fall23.onrender.com',
+  host: 'cse341fall23.onrender.com',
   schemes: ['http'],
 };
 
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./routes/index.js'];
-
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
 
 swaggerAutogen(outputFile, endpointsFiles, doc)
