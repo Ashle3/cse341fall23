5dd56b00e5be8d25990309f23298cfb34ec46514
Ashle3 <ashleeb3@byui.edu>
2023-10-07 22:08:42 -0500

    changing host to render

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'Contacts API',
     description: 'List of people I know.',
   },
-  host: 'localhost:8080',
+  host: 'https://cse341fall23.onrender.com',
   schemes: ['http'],
 };
 
