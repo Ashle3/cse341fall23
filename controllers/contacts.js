09650bf25f59884eba469c2a0299f1bfae3771f4
Ashle3 <ashleeb3@byui.edu>
2023-10-20 19:43:37 -0500

    error handling complete

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,24 +2,37 @@ const mongodb = require('../db/connection');
 //const { put } = require('../routes/contacts');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res, next) => {
-    const result = await mongodb.getDb().db('contacts').collection('contacts').find();
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
-  };
-  
-const getSingle = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
+const getAll = (req, res) => {
+    mongodb
       .getDb()
       .db('contacts')
       .collection('contacts')
-      .find({ _id: userId });
-    result.toArray().then((lists) => {
+      .find()
+      .toArray((err, lists) => {
+        if (err) {
+          res.status(400).json({ message: err });
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+      });
+  };
+  
+const getSingle = (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id.');
+  }
+  const userId = new ObjectId(req.params.id);
+  mongodb
+    .getDb()
+    .db('contacts')
+    .collection('contacts')
+    .find({ _id: userId })
+    .toArray((err, result) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
       res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists[0]);
+      res.status(200).json(result[0]);
     });
 };
 
